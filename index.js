e70a5be823f06a88e2a769f9b0cef37314470476
Carly <clig2@cam.ac.uk>
2019-06-29 14:28:58 +0100

    Added choice of operations with if...else

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const readline = require('readline-sync');
 console.log('This is the calculator!');
 
+console.log('Please enter an operator:');
+const operator = readline.prompt();
+
 console.log('Please enter a number:');
 const strnum1 = readline.prompt();
 const num1 = +strnum1;
@@ -9,6 +12,19 @@ console.log('Please enter another number:');
 const strnum2 = readline.prompt();
 const num2 = +strnum2;
 
-const answer = num1 * num2;
+let answer = 0;
+if (operator == '+'){
+    answer = num1 + num2;
+} else if (operator == '-'){
+    answer = num1 - num2;
+} else if (operator == '*'){
+    answer = num1 * num2;
+} else if (operator == '/'){
+    answer = num1 / num2;
+} else {
+    console.log(operator + ' is an invalid operator.');
+    answer = 'undefined';
+}
 
-console.log('The product of these numbers is ' + answer + '.');
+
+console.log('The answer is ' + answer + '.');
