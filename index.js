f7f2219f12beb5c06b269d38df66e5d7cb6000af
Carly <clig2@cam.ac.uk>
2019-06-29 22:33:41 +0100

    Added functions to make sure calculator keeps running and we have entered a number

--- index.js
+++ index.js
@@ -1,44 +1,85 @@
 const readline = require('readline-sync');
-console.log('This is the calculator!');
 
-console.log('Please enter an operator:');
-const operator = readline.prompt();
+function printWelcomeMessage(){
+    console.log('This is the calculator!');
+}
 
-console.log('How many numbers do you want to ' + operator + '?')
-const arraysize = readline.prompt();
-var arr = Array(+arraysize)
+function howManyNumbers(op){
+    console.log('How many numbers do you want to ' + op + '?')
+    const arraysize = readline.prompt();
+    var arr = Array(+arraysize);
+    return arr
+}
 
-for (var i=0; i<arr.length; i++ ){
-    console.log('Please enter number ' + (i+1) + ':');
-    arr[i] = +readline.prompt();
+function chooseOperator(){
+    console.log('Please enter an operator:');
+    const operator = readline.prompt();
+    return operator
+}
+
+function printAnswer(answer){
+    console.log('The answer is ' + answer + '.');
+}
+
+function enterNumber(arr, index){
+    console.log('Please enter number ' + (index+1) + ':');
+    let response = readline.prompt();
+    number = +response;
+    while (isNaN(number)){
+        console.log('Please enter number ' + (index+1) + ':');
+        response = readline.prompt();
+        number = +response;
     }
-let answer = arr[0];
-switch(operator){
-    case '+':
-        for (var i=1; i<arr.length; i++ ){
-            answer += arr[i]
-            }
-        break;
-    case '-':
-        for (var i=1; i<arr.length; i++ ){
-            answer -= arr[i]
-            }
-        break;
-    case '*':
-        for (var i=1; i<arr.length; i++ ){
-            answer *= arr[i]
-            }
-        break;
-    case '/':
-        for (var i=1; i<arr.length; i++ ){
-            answer /= arr[i]
-            }
-        break;
-    default:
-        console.log(operator + ' is an invalid operator.');
-        answer = 'undefined';
-        break;
+    arr[index] = number;
+    return arr;
+}
+
+function errorMessage(operator){
+    console.log(operator + ' is an invalid operator.');
+    answer = 'undefined';
+    return answer;
 }
 
 
-console.log('The answer is ' + answer + '.');
+function performOneCalculation(){
+    var operator = chooseOperator()
+    var arr = howManyNumbers(operator)
+
+    for (var i=0; i<arr.length; i++ ){
+        arr = enterNumber(arr, i);
+        }
+    let answer = arr[0];
+    switch(operator){
+        case '+':
+            for (var i=1; i<arr.length; i++ ){
+                answer += arr[i]
+                }
+            break;
+        case '-':
+            for (var i=1; i<arr.length; i++ ){
+                answer -= arr[i]
+                }
+            break;
+        case '*':
+            for (var i=1; i<arr.length; i++ ){
+                answer *= arr[i]
+                }
+            break;
+        case '/':
+            for (var i=1; i<arr.length; i++ ){
+                answer /= arr[i]
+                }
+            break;
+        default:
+            answer = errorMessage(operator);
+    }
+    printAnswer(answer);
+}
+
+
+
+printWelcomeMessage()
+while (true)
+    performOneCalculation();
+
+
